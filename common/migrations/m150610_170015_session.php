2c38628a9106690afc4a7dbff6beffb048312883
Revin Roman <xgismox@gmail.com>
2015-06-10 21:10:56 +0300

    Update

--- common/migrations/m150610_170015_session.php
+++ common/migrations/m150610_170015_session.php
@@ -8,9 +8,10 @@ class m150610_170015_session extends \common\components\Migration
     public function up()
     {
         $this->createTable('{{%session}}', [
-            'id' => 'CHAR(40) NOT NULL  PRIMARY KEY',
+            'id' => 'CHAR(40) NOT NULL',
             'expire' => Schema::TYPE_INTEGER,
             'data' => 'LONGBLOB',
+            'PRIMARY KEY (id)',
         ]);
 
         $this->createIndex('idx_expire', '{{%session}}', ['expire']);
