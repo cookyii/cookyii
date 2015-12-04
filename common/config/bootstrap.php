3c214df3fb283c66e8f6fb4a3b9fcc7abdb8ba12
Revin Roman <xgismox@gmail.com>
2015-12-04 19:07:31 +0300

    Update

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -17,10 +17,13 @@ Yii::setAlias('upload', '@base/upload');
 Yii::setAlias('console', '@base/console-app');
 
 Yii::setAlias('frontend', '@base/frontend-app');
+Yii::setAlias('frontend/assets', '@base/frontend-assets');
 Yii::setAlias('frontend/modules', '@base/frontend-modules');
 
 Yii::setAlias('backend', '@base/backend-app');
+Yii::setAlias('backend/assets', '@base/backend-assets');
 Yii::setAlias('backend/modules', '@base/backend-modules');
 
 Yii::setAlias('crm', '@base/crm-app');
+Yii::setAlias('crm/assets', '@base/crm-assets');
 Yii::setAlias('crm/modules', '@base/crm-modules');
