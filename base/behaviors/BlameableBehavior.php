739ebd6bed59de61e4e0010a4a5db15ea574180e
Revin Roman <xgismox@gmail.com>
2016-05-29 13:02:45 +0300

    Add BlameableBehavior

---
+++ base/behaviors/BlameableBehavior.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * BlameableBehavior.php
+ * @author Revin Roman
+ * @link https://rmrevin.com
+ */
+
+namespace cookyii\behaviors;
+
+/**
+ * Class BlameableBehavior
+ * @package cookyii\behaviors
+ */
+class BlameableBehavior extends \yii\behaviors\BlameableBehavior
+{
+
+}
