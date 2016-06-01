2d17441474ef553d483ca9e4ea9568638d38bc31
Revin Roman <xgismox@gmail.com>
2016-06-01 23:51:32 +0300

    Add proxy to media model

---
+++ resources/Media.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Media.php
+ * @author Revin Roman
+ * @link https://rmrevin.com
+ */
+
+namespace resources;
+
+/**
+ * Class Media
+ * @package resources
+ */
+class Media extends \cookyii\modules\Media\resources\Media
+{
+
+}
