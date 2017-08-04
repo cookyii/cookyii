26735c39e2ad5c92e3feee2fc4a6101aa5cabd29
Revin Roman <xgismox@gmail.com>
2017-08-04 10:08:50 +0300

    Add account change-password command

--- modules/Account/commands/AccountCommand.php
+++ modules/Account/commands/AccountCommand.php
@@ -58,9 +58,9 @@ class AccountCommand extends \yii\console\Controller
         /** @var AccountModel $Account */
         $Account = \Yii::createObject(AccountModel::class);
         $Account->setAttributes([
-            'name' => $name,
-            'email' => $email,
-            'password' => $pass,
+            'name'         => $name,
+            'email'        => $email,
+            'password'     => $pass,
             'activated_at' => time(),
         ]);
 
@@ -69,9 +69,60 @@ class AccountCommand extends \yii\console\Controller
             F::AuthManager()->assign(RbacFactory::Role($roles['user']), $Account->id);
             F::AuthManager()->assign(RbacFactory::Role($roles['admin']), $Account->id);
 
-            $this->stdout("User have been successfully added\n", Console::FG_GREEN);
+            $this->stdout("User have been successfully added.\n", Console::FG_GREEN);
         } else {
-            $this->stdout("ERROR creating user\n", Console::FG_RED);
+            $this->stdout("ERROR creating user.\n", Console::FG_RED);
+
+            $error = array_shift($Account->getFirstErrors());
+            if (!empty($error)) {
+                $this->stdout("\t> {$error}\n", Console::FG_RED);
+            }
+
+            return static::EXIT_CODE_ERROR;
+        }
+
+        return static::EXIT_CODE_NORMAL;
+    }
+
+    /**
+     * @param string $email
+     * @param string $pass
+     * @return mixed
+     * @throws \Exception
+     */
+    public function actionChangePassword($email = '', $pass = '')
+    {
+        if (empty($email)) {
+            $email = $this->prompt('Enter user email:', [
+                'required' => true,
+            ]);
+        }
+
+        if (empty($pass)) {
+            $pass = $this->prompt('Enter user password:', [
+                'required' => true,
+            ]);
+        }
+
+        /** @var AccountModel $Account */
+        $AccountModel = \Yii::createObject(AccountModel::class);
+        $Account = $AccountModel::find()
+                                ->byEmail($email)
+                                ->one();
+
+        if (empty($Account)) {
+            throw new \Exception("User `$email` not found.");
+        }
+
+        $Account->setAttributes([
+            'password' => $pass,
+        ]);
+
+        $Account->save();
+        if (!$Account->hasErrors()) {
+            $this->stdout("User password have been successfully updated.\n", Console::FG_GREEN);
+        } else {
+            $this->stdout("ERROR updating user password.\n", Console::FG_RED);
 
             $error = array_shift($Account->getFirstErrors());
             if (!empty($error)) {
